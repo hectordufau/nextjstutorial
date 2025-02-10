2df40c252fd103ceba4bc4789f71d537f447cc5a
hectordufau <contato@hectordufau.com.br>
2025-02-10 19:58:12 -0300

    loading

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -6,7 +6,8 @@ const Home = () => {
       <h1>Welcome home!</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/products">Products</Link>
-      <Link href="/">Home</Link>
+      <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+      <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
     </>
   );
 };
